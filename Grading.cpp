b1af152cc147e9b482a030accb3857c8108d6a33
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-02-11 15:00:31 +0700

    finish Garading

--- Grading.cpp
+++ Grading.cpp
@@ -7,6 +7,32 @@ int main(){
 	scanf("%d",&b);
 	printf("Enter c: ");
 	scanf("%d",&c);
-	
+	score = a+b+c;
+	if(score>=0&&score<=100)
+	{
+		 if(score>=80)
+			printf("A");
+
+		else if(score>=75)
+			printf("B+");
+
+		else if(score>=70)
+			printf("B");
+
+		else if(score>=65)
+			printf("C+");
+
+		else if(score>=60)
+			printf("C");
+
+		else if(score>=55)
+			printf("D+");
+
+		else if(score>=50)
+			printf("D");
+		else
+			printf("F");
+	}
+	else printf("Error!!!");
 	return 0;
 }
